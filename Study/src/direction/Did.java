0429b67d3d3997e010a612e1fa36a6cde07c2bb0
User <User@DESKTOP-66D904S>
2017-03-10 20:13:58 +0300

    sdfs

--- Study/src/direction/Did.java
+++ Study/src/direction/Did.java
@@ -8,12 +8,14 @@ public class Did {
 		System.out.println("Input a word at 5 letters...");
 		Scanner scanner = new Scanner(System.in);
 		String name = scanner.nextLine();
-		if(name.charAt(0) == name.charAt(4)){
-			if(name.charAt(1) == name.charAt(3)){
-				System.out.println("word "+name+" is poliardrom");
-			}
-		}else{
-		System.out.println(name + " isn`t poliardrom");
+		String name2 = null;
+		name2=name;
+		name2.toLowerCase();
+		if(name2.charAt(0) == name2.charAt(4) && name2.charAt(1) == name2.charAt(3)){
+				System.out.println("word "+name2.toLowerCase()+" is poliardrom");
+		}
+		else{
+				System.out.println(name + " isn`t poliardrom");
 		}
 	}
 
