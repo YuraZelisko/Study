519af2eee70aa2194760e3b6c81e4f2ef68824ad
Canon MF5700 <Canon MF5700@adobe-soft.com.ua-test>
2017-03-10 14:16:37 +0200

    fas

--- Study/src/year/Main.java
+++ Study/src/year/Main.java
@@ -12,58 +12,66 @@ public class Main {
 		
 		for (int j = 0; j < month.length; j++) {
 				if(input.equalsIgnoreCase(month[j].name())){
-					System.out.println(input + " that month avalible");
-					
-					System.out.println("all months woth same season");
+					System.out.println(input + " is avalible");
+					System.out.println(" ");
+					System.out.println("All months with same season...");
 					for (int i = 0; i < month.length; i++) {
 						if(month[j].getSeason().equals(month[i].getSeason())){
 							System.out.println(month[i].name());
 						}
 					}
-					System.out.println("Month days equals");
+					System.out.println(" ");
+					System.out.println("Month days equals...");
 					for (int i = 0; i < month.length; i++) {
 						if(month[j].getDay()==month[i].getDay()){
 							System.out.println(month[i]);
 						}
 					}
-					System.out.println("Month days less than input");
+					System.out.println(" ");
+					System.out.println("Month days less than input...");
 					for (int i = 0; i < month.length; i++) {
 						if(month[i].getDay()<month[j].getDay()){
 							System.out.println(month[i]);
 						}
 					}
-					System.out.println("Month days grow than input");
+					System.out.println(" ");
+					System.out.println("Month days grow than input...");
 					for (int i = 0; i < month.length; i++) {
 						if(month[j].getDay()<month[i].getDay()){
 							System.out.println(month[i]);
 						}
 					}
-					System.out.println("Next season");
+					System.out.println(" ");
+					System.out.println("Next season...");
 					for (int i = 0; i < month.length; i++) {
 						if(month[j].getSeason().equals(month[i].getSeason())){
 							System.out.println(month[i+3].getSeason());break;
 						}
 					}
-					System.out.println("Previos season");
+					System.out.println(" ");
+					System.out.println("Previos season...");
 					for (int i = 0; i < month.length; i++) {
 						if(month[j].getSeason().equals(month[i].getSeason())){
 							System.out.println(month[i-1].getSeason());
 							break;
 						}
 					}
-					System.out.println("Pariry month ?");
+					System.out.println(" ");
+					System.out.println("Pariry month ?...");
 					for (int i = 0; i < month.length; i++) {
 						if((month[j].getDay()%2==0)){
-							System.out.println(month[j]+" month is Parity ");break;
+							System.out.println(month[j]+" month is Parity");break;
 						}else {
-							System.out.println(month[j]+" month is Non parity ");break;
+							System.out.println(month[j]+" month is Non parity");break;
 					}
 				}
+					System.out.println(" ");
 					System.out.println("You see parirty months...");
 					for (int i = 0; i < month.length; i++) {
 						if((month[i].getDay()%2==0)){
 							System.out.println(month[i]+" parity month");
 						}}
+					System.out.println(" ");
 					System.out.println("You see Non parirty months...");
 					for (int i = 0; i < month.length; i++) {
 							if((month[i].getDay()%2!=0)){
