e18cd3172909a5b4b89955af4faf5ad3fe9361b7
Canon MF5700 <Canon MF5700@adobe-soft.com.ua-test>
2017-03-10 13:59:37 +0200

    asfg

--- Study/src/year/Main.java
+++ Study/src/year/Main.java
@@ -1,6 +1,7 @@
 package year;
 
 import java.util.Scanner;
+import java.util.jar.Attributes.Name;
 
 public class Main {
 	public static void main(String[] args) {
@@ -12,12 +13,71 @@ public class Main {
 		for (int j = 0; j < month.length; j++) {
 				if(input.equalsIgnoreCase(month[j].name())){
 					System.out.println(input + " that month avalible");
+					
+					System.out.println("all months woth same season");
+					for (int i = 0; i < month.length; i++) {
+						if(month[j].getSeason().equals(month[i].getSeason())){
+							System.out.println(month[i].name());
+						}
+					}
+					System.out.println("Month days equals");
+					for (int i = 0; i < month.length; i++) {
+						if(month[j].getDay()==month[i].getDay()){
+							System.out.println(month[i]);
+						}
+					}
+					System.out.println("Month days less than input");
+					for (int i = 0; i < month.length; i++) {
+						if(month[i].getDay()<month[j].getDay()){
+							System.out.println(month[i]);
+						}
+					}
+					System.out.println("Month days grow than input");
+					for (int i = 0; i < month.length; i++) {
+						if(month[j].getDay()<month[i].getDay()){
+							System.out.println(month[i]);
+						}
+					}
+					System.out.println("Next season");
+					for (int i = 0; i < month.length; i++) {
+						if(month[j].getSeason().equals(month[i].getSeason())){
+							System.out.println(month[i+3].getSeason());break;
+						}
+					}
+					System.out.println("Previos season");
+					for (int i = 0; i < month.length; i++) {
+						if(month[j].getSeason().equals(month[i].getSeason())){
+							System.out.println(month[i-1].getSeason());
+							break;
+						}
+					}
+					System.out.println("Pariry month ?");
+					for (int i = 0; i < month.length; i++) {
+						if((month[j].getDay()%2==0)){
+							System.out.println(month[j]+" month is Parity ");break;
+						}else {
+							System.out.println(month[j]+" month is Non parity ");break;
+					}
+				}
+					System.out.println("You see parirty months...");
+					for (int i = 0; i < month.length; i++) {
+						if((month[i].getDay()%2==0)){
+							System.out.println(month[i]+" parity month");
+						}}
+					System.out.println("You see Non parirty months...");
+					for (int i = 0; i < month.length; i++) {
+							if((month[i].getDay()%2!=0)){
+							System.out.println(month[i]+" non parity month");
+							}
+					}
 					exist = true;
 					break;
 				}
 			}
 		if(exist==false){
 			System.out.println("uncorrect month name");
+
 		}
 	} 
 }
+
--- Study/src/year/Month.java
+++ Study/src/year/Month.java
@@ -4,7 +4,7 @@ public enum Month{
 	JANUARY(Season.WINTER, 31), FERBRUARY(Season.WINTER, 28), MARCH(Season.SPRING, 31), 
 	APRIL(Season.SPRING, 30), MAY(Season.SPRING, 31), JUNE(Season.SUMMER, 30), 
 	JULY(Season.SUMMER, 31), AUGUST(Season.SUMMER, 31), SEPTEMBER(Season.AUTUMN, 30), 
-	OCTOUBER(Season.AUTUMN,31), NOVEMBER(Season.AUTUMN, 30), DECEMBER(Season.AUTUMN, 31);
+	OCTOUBER(Season.AUTUMN,31), NOVEMBER(Season.AUTUMN, 30), DECEMBER(Season.WINTER, 31);
 	
 	Season season;
 	int day;
