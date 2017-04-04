d89fbe0c75933ce18112ca50b9dc6b0befc5fa43
YuraZelisko <meteoritKIDS@gmail.com>
2017-04-04 13:22:36 +0300

    xvxv

--- Study/src/zooparty/Main.java
+++ Study/src/zooparty/Main.java
@@ -1,16 +1,16 @@
 package zooparty;
 
-import java.util.HashMap;
+//import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 	public static Scanner scanner = new Scanner(System.in);
 	public static void main(String[] args) {
-		
-	
-		Map map = new HashMap<>();
-		
+		System.out.println(args.length);
+//	
+//		Map map = new HashMap<>();
+//		
 		
 	}
 
