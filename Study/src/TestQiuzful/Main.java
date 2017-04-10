53ea3364508a740c1a284a398dfdacfe17ac16fa
User <User@DESKTOP-66D904S>
2017-04-10 20:11:55 +0300

    11

--- Study/src/TestQiuzful/Main.java
+++ Study/src/TestQiuzful/Main.java
@@ -1,15 +1,13 @@
 package TestQiuzful;
 
+import java.util.Formatter;
+
 public class Main {
 
 	public static final void main(String... args) {
 		
-	int a = 012;
-	int b = 20;
-	System.out.println(a*b);
-	
-				
-			}
+		
+	}
 	
 
 }
